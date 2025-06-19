56c35b2c06f6ac784d061a3f89eefd43b1544aab
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-06-19 12:18:55 +0530

    Added Clerk for auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,21 @@
-import Image from "next/image";
-import { Button } from "@/components/ui/button"
-import { Card } from "@/components/ui/card";
+import {
+  SignedIn,
+  SignedOut,
+  SignInButton,
+  SignUpButton,
+  UserButton,
+} from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <h1>
-        Home
-        <Button>Click me</Button>
-        <Card>This is a card</Card>
-      </h1>
+    <div className="m-10">
+      <SignedOut>
+        <SignInButton />
+        <SignUpButton />
+      </SignedOut>
+      <SignedIn>
+        <UserButton />
+      </SignedIn>
     </div>
-  )
+  );
 }
