bfb2ef9be9de01f6a816c7005a2f4b5adfcc05b7
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-06-19 17:55:33 +0530

    Added convex, schema and configure stream

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,8 @@
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
 
 export default function Home() {
   return (
     <div className="m-10">
-      <SignedOut>
-        <SignInButton />
-        <SignUpButton />
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
+      HomePage
     </div>
   );
 }
